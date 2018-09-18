3eaef0ed02f8013b7c729e9c9449fa5110987e4e
Joel Quinones <joelquinones@joels-mbp.attlocal.net>
2018-09-17 23:21:44 -0500

    patch 2005

---
+++ buildScripts/distServer.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import compression from 'compression'
+/* eslint-disable no-console */
+
+const port = 3000;
+const app = express();
+
+app.use(compression());
+app.use(express.static('dist'));
+
+app.get('/', function(req, res) {
+		res.sendFile(path.join(__dirname,'../dist/index.html'));
+});
+
+app.get('/users', function(req, res){
+	//Hard coding for simplicity. This would hit actual DB.
+res.json([
+	{"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@aol.com"},
+	{"id": 2,"firstName":"Todd","lastName":"Birth","email":"Toad@aol.com"},
+	{"id": 3,"firstName":"Jessica","lastName":"Yes","email":"Yes@yahoo.com"}
+	]);
+});
+
+
+app.listen(port, function(err){
+		if (err) {
+			console.log(err);
+		} else {
+			open('http://localhost:' + port);
+		}
+});
