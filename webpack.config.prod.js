9f094bb3958f7f1c15a483bb19d6eb31cc2e750c
Joel Quinones <joelquinones@joels-mbp.attlocal.net>
2018-09-17 23:41:05 -0500

    patch 2006

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
   debug: true,
@@ -15,6 +16,13 @@ export default {
     filename: 'bundle.js'
   },
   plugins: [
+
+			//create html file that include referece to bundle.js
+			new HtmlWebpackPlugin({
+				template: 'src/index.html',
+				inject: true
+			}),
+
 			//Eleminate duplicate packages when generating bundle.
 			new webpack.optimize.DedupePlugin(),
 
