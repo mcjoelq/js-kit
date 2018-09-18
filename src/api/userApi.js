0a99cd87c6e6fd04e4b96a9351800c64aabde79b
Joel Quinones <joelquinones@joels-mbp.attlocal.net>
2018-09-17 21:14:01 -0500

    patch 2002

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,11 +1,26 @@
 import 'whatwg-fetch';
+import getBaseUrl from './baseUrl';
+
+const baseUrl = getBaseUrl();
 
 export function getUsers(){
 	return get('users');
 }
 
+export function deleteUser(id) {
+	return del(`users/${id}`);
+}
+
 function get(url){
-	return fetch(url).then(onSucess, onError);
+	return fetch(baseUrl + url).then(onSucess, onError);
+}
+
+function del(url){
+	const request = new Request(baseUrl + url, {
+		method: 'DELETE'
+	});
+
+	return fetch(request).then(onSucess, onError);
 }
 
 function onSucess(response){
