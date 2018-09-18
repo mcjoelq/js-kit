ca979a5e7b630371aa96ec0752b9fcebb49760ee
Joel Quinones <joelquinones@joels-mbp.attlocal.net>
2018-09-17 21:28:03 -0500

    patch 2003

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 
-import {getUsers} from './api/userApi';
+import {getUsers, deleteUser} from './api/userApi';
 
 // populate table of users via API call
 getUsers().then(result => {
@@ -17,4 +17,19 @@ getUsers().then(result => {
 	})
 
 	global.document.getElementById('users').innerHTML = usersBody;
+
+const deleteLinks = global.document.getElementByClassName('deleteUser');
+
+//Must use array.from to create a real array from a DOM selection
+// getElementByClassName only returns an "array like" object
+Array.from(deleteLinks, link => {
+	link.onclick = function(event){
+		const element = event.target;
+		event.preventDefault();
+		deleteUser(element.attributes["data-id"].value);
+		const row = element.parentNode.parentNode;
+		row.parentNode.removeChild(row);
+		};
+	});
+
 });
